c27cbffc3da27324a91f6eac162a33bb68efa740
nikolay <nazarov.nikolay15@wb.ru>
2024-05-19 14:04:09 +0300

    Все готово, но я хочу попробовать передать обьект дата класса в интент

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,24 +1,40 @@
 package otus.gpb.homework.activities.receiver
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+
 
 class ReceiverActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
-        val data1 = intent?.getStringExtra("data1")
-        val data2 = intent.getStringExtra("data2")
-        val data3 = intent.getStringExtra("data3")
+        val titledata = intent?.getStringExtra("title")
+        val yeardata = intent.getStringExtra("year")
+        val descriptiondata = intent.getStringExtra("description")
+
 
         val posterImageView = findViewById<ImageView>(R.id.posterImageView)
         val titleTextView = findViewById<TextView>(R.id.titleTextView)
-        val descriptionTextView = findViewById<TextView>(R.id.descriptionTextView)
         val yearTextView = findViewById<TextView>(R.id.yearTextView)
+        val descriptionTextView = findViewById<TextView>(R.id.descriptionTextView)
+
+        if(titledata == "Славные парни")
+            posterImageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.niceguys))
+        else
+            posterImageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.interstellar))
+
+        // val a = Context.getDrawable() - не работает, не понятно почему
+        titleTextView.text = titledata
+        yearTextView.text = yeardata
+        descriptionTextView.text = descriptiondata
+
 
 
     }
--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+
+
 class SenderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
